08bdd58d165689f8b6bdb7494ac0b7b612bd1ae5
Ivan Zlatanov <ivan@zlatanov.net>
2018-09-03 17:45:29 +0300

    Created.

---
+++ Server/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Server
+{
+    public class Program
+    {
+        public static void Main( string[] args )
+        {
+            WebHost.CreateDefaultBuilder( args )
+                   .UseStartup<Program>()
+                   .Build()
+                   .Run();
+        }
+
+
+        public Program( IConfiguration configuration, ILoggerFactory logger )
+        {
+            Configuration = configuration;
+            Logger = logger.CreateLogger( "WebSockets" );
+        }
+
+        public IConfiguration Configuration { get; }
+        public ILogger Logger { get; }
+
+
+        public void ConfigureServices( IServiceCollection services )
+        {
+        }
+
+
+        public void Configure( IApplicationBuilder app, IHostingEnvironment env )
+        {
+            app.Run( RunAsync );
+        }
+
+
+        private async Task RunAsync( HttpContext context )
+        {
+            // Detect if an opaque upgrade is available. If so, add a websocket upgrade.
+            var upgradeFeature = context.Features.Get<IHttpUpgradeFeature>();
+
+            if ( upgradeFeature != null )
+            {
+                var request = new WebSocketRequest( context, upgradeFeature );
+
+                if ( request.IsWebSocketRequest )
+                {
+                    var stream = await request.UpgradeAsync();
+                    var buffer = new Byte[ 1024 ];
+
+                    while ( true )
+                    {
+                        var byteCount = await stream.ReadAsync( buffer );
+
+                        if ( byteCount == 0 )
+                        {
+                            Logger.Log( LogLevel.Information, "Aborted" );
+                            break;
+                        }
+                        else
+                        {
+                            Debug.Assert( byteCount == 2 );
+                            Debug.Assert( ( buffer[ 0 ] & 0b0000_1111 ) == 0x9/*Ping*/ );
+
+                            Logger.Log( LogLevel.Information, "Ping received" );
+
+                            // Reply with pong
+                            await stream.WriteAsync( new Byte[] { 0b1000_1010, 0 } );
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ Server/WebSocketRequest.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+
+namespace Server
+{
+    internal static class WebSocketHeaders
+    {
+        public const String Connection = "Connection";
+        public const String ConnectionUpgrade = "Upgrade";
+        public const String Host = "Host";
+        public const String SecWebSocketAccept = "Sec-WebSocket-Accept";
+        public const String SecWebSocketExtensions = "Sec-WebSocket-Extensions";
+        public const String SecWebSocketKey = "Sec-WebSocket-Key";
+        public const String SecWebSocketProtocol = "Sec-WebSocket-Protocol";
+        public const String SecWebSocketVersion = "Sec-WebSocket-Version";
+        public const String Upgrade = "Upgrade";
+        public const String UpgradeWebSocket = "websocket";
+
+        public const String SupportedVersion = "13";
+    }
+
+
+    internal sealed class WebSocketRequest
+    {
+        private readonly HttpContext _context;
+        private readonly IHttpUpgradeFeature _upgradeFeature;
+
+
+        public WebSocketRequest( HttpContext context, IHttpUpgradeFeature upgradeFeature )
+        {
+            _context = context;
+            _upgradeFeature = upgradeFeature;
+
+            if ( upgradeFeature.IsUpgradableRequest )
+            {
+                IsWebSocketRequest = CheckSupportedWebSocketRequest();
+            }
+        }
+
+
+        public Boolean IsWebSocketRequest { get; }
+
+
+        public async Task<Stream> UpgradeAsync()
+        {
+            var response = _context.Response;
+
+            response.Headers[ WebSocketHeaders.Connection ] = "Upgrade";
+            response.Headers[ WebSocketHeaders.ConnectionUpgrade ] = "websocket";
+            response.Headers[ WebSocketHeaders.SecWebSocketAccept ] = CreateResponseKey();
+
+            return await _upgradeFeature.UpgradeAsync();
+        }
+
+
+        private Boolean CheckSupportedWebSocketRequest()
+        {
+            var comparer = StringComparer.OrdinalIgnoreCase;
+
+            var request = _context.Request;
+            var version = request.Headers[ WebSocketHeaders.SecWebSocketVersion ];
+            var key = request.Headers[ WebSocketHeaders.SecWebSocketKey ];
+
+            if ( !comparer.Equals( request.Method, "GET" ) )
+            {
+                return false;
+            }
+            else if ( !comparer.Equals( request.Headers[ WebSocketHeaders.ConnectionUpgrade ], "websocket" ) )
+            {
+                return false;
+            }
+            else if ( !WebSocketHeaders.SupportedVersion.Equals( version ) || !IsRequestKeyValid( key ) )
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+
+        private static Boolean IsRequestKeyValid( String value )
+        {
+            if ( String.IsNullOrWhiteSpace( value ) )
+            {
+                return false;
+            }
+
+            try
+            {
+                return Convert.FromBase64String( value ).Length == 16;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+
+        private String CreateResponseKey()
+        {
+            String key = _context.Request.Headers[ WebSocketHeaders.SecWebSocketKey ];
+
+            // "The value of this header field is constructed by concatenating /key/, defined above in step 4
+            // in Section 4.2.2, with the String "258EAFA5-E914-47DA-95CA-C5AB0DC85B11", taking the SHA-1 hash of
+            // this concatenated value to obtain a 20-Byte value and base64-encoding"
+            // https://tools.ietf.org/html/rfc6455#section-4.2.2
+            using ( var sha1 = SHA1.Create() )
+            {
+                var mergedBytes = Encoding.UTF8.GetBytes( key + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11" );
+                var hashedBytes = sha1.ComputeHash( mergedBytes );
+
+                return Convert.ToBase64String( hashedBytes );
+            }
+        }
+    }
+}
